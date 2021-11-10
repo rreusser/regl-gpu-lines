d17ad4c9f76d90e3d452128bcd29295ab3229598
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2021-11-10 12:46:07 -0800

    Pointless example

---
+++ examples/hexgrid.js
@@ -0,0 +1,146 @@
+function mat2create() { return [1, 0, 0, 1]; }
+function mat2rotate(out, a, rad) {
+  var a0 = a[0], a1 = a[1], a2 = a[2], a3 = a[3];
+  var s = Math.sin(rad);
+  var c = Math.cos(rad);
+  out[0] = a0 *  c + a2 * s;
+  out[1] = a1 *  c + a3 * s;
+  out[2] = a0 * -s + a2 * c;
+  out[3] = a1 * -s + a3 * c;
+  return out;
+}
+
+const regl = createREGL({
+  extensions: ['ANGLE_instanced_arrays'],
+  attributes: {antialias: false},
+});
+
+// Instantiate a command for drawing lines
+const drawLines = reglLines(regl, {
+  vert: `
+    precision highp float;
+
+    #pragma lines: attribute vec3 xy;
+    #pragma lines: position = getPosition(xy);
+    uniform mat2 transform;
+    uniform vec2 aspect;
+    vec4 getPosition(vec3 xy) {
+      return vec4((transform * (xy.xy * 0.03)) * aspect, 0, 1);
+    }
+
+    #pragma lines: varying float t = getT(xy);
+    float getT(vec3 xy) { return xy.z; }
+
+
+    // Return the line width from a uniorm
+    #pragma lines: width = getWidth();
+    uniform lowp float width;
+    float getWidth() {
+      return width;
+    }`,
+  frag: `
+    precision lowp float;
+    varying float t;
+    varying vec2 lineCoord;
+    uniform lowp float width;
+    uniform float pixelRatio;
+    float linearstep(float a, float b, float x) { return clamp((x - a) / (b - a), 0.0, 1.0); }
+    void main () {
+      float sdf = width * abs(lineCoord.y);
+      float alpha = linearstep(width, width - 1.0, sdf);
+      float c = fract(t);
+      if (c > 1.0 / 3.0) {
+        discard;
+      } else {
+        gl_FragColor = vec4(vec3(1), alpha);
+      }
+    }`,
+
+  // Multiply the width by the pixel ratio for consistent width
+  uniforms: {
+    transform: regl.prop('transform'),
+    pixelRatio: regl.context('pixelRatio'),
+    width: (ctx, props) => ctx.pixelRatio * props.width,
+    aspect: ctx => ctx.viewportWidth > ctx.viewportHeight
+      ? [ctx.viewportHeight / ctx.viewportWidth, 1]
+      : [1, ctx.viewportWidth / ctx.viewportHeight],
+  },
+
+  blend: {
+    enable: true,
+    func: {
+      srcRGB: 'src alpha',
+      srcAlpha: 1,
+      dstRGB: 'one minus src alpha',
+      dstAlpha: 1
+    }
+  },
+
+  depth: {enable: false}
+});
+
+let xy = [];
+
+const yShift = Math.sin(Math.PI / 3);
+const unit = [[0, 0], [1 - Math.cos(Math.PI / 3), Math.sin(Math.PI / 3)]]
+let c = 0;
+const t = [];
+const rows = 30;
+const cols = 25;
+
+for (let k = -cols; k <=cols; k++) {
+  for (let j of [-1, 1]) {
+    for (let i = -rows; i <= rows; i++) {
+      xy.push(unit.map(([x, y]) => [
+        x + j * 0.75 + k * 3,
+        (y + i * yShift * 2) * j
+          - j * yShift * 0.5,
+        -1,
+      ]));
+    }
+  }
+}
+
+for (let j = rows; j >= -rows; j--) {
+  const x1 = cols * 3 + 1.25;
+  const e = 0.5;
+  const shift = 1.5;
+  const extent = 2 * cols + 1;
+  xy.push([
+    [x1, j, extent],
+    [-x1 - e, j, 0],
+    [-x1 - e + shift, j - 0.5, 0],
+    [x1 + shift, j - 0.5, extent]
+  ].map(([x, y, z]) => [
+    x,
+    (y + 0.25) * yShift * 2,
+    z
+  ]));
+}
+
+// Center it about zero
+xy = xy.flat().map(([x, y, z]) => [x - 0.25, y - yShift * 0.5, z]);
+
+// Set up the data to be drawn. Note that we preallocate buffers and don't create
+// them on every draw call.
+const lineData = {
+  width: 1,
+  roundResolution: 1,
+  join: 'round',
+  cap: 'square',
+  vertexCount: xy.length,
+  vertexAttributes: {
+    xy: regl.buffer(xy),
+  }
+};
+
+
+
+regl.frame(({time}) => {
+  regl.poll();
+  regl.clear({color: [0.2, 0.2, 0.2, 1]});
+  drawLines([
+    {...lineData, transform: mat2create()},
+    {...lineData, transform: mat2rotate([], mat2create(), time * 0.1) }
+  ]);
+});
